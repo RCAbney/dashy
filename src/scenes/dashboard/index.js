66caaf97199d602ccb351d9005485bf8907c3145
RCAbney <ryan@rcabney.com>
2022-11-27 14:57:12 -0500

    initial commit

---
+++ src/scenes/dashboard/index.js
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material";
+import React from "react";
+import Header from "../../components/Header";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Box m="20px">
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export default Dashboard;
